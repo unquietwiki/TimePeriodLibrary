489e6d160c9a964a80ebc8962ecad3a2127b4910
Michael Adams <unquietwiki@gmail.com>
2017-11-24 17:17:05 -0800

    Changing the main "HasInside" to use strict DateTime.Compare rules, appears to fix issue #17

--- TimePeriod/TimePeriodCalc.cs
+++ TimePeriod/TimePeriodCalc.cs
@@ -7,9 +7,10 @@
 // copyright  : (c) 2011-2012 by Itenso GmbH, Switzerland
 // --------------------------------------------------------------------------
 
-// Attempt to fix Issue #17: use ticks, and change the comparison.
+// Attempt to fix Issue #17: use ticks, and change the comparison. Ended up using DateTime.Compare to resolve this.
 // https://stackoverflow.com/questions/5672862/check-if-datetime-instance-falls-in-between-other-two-datetime-objects
 // https://github.com/augustoclaro/datetimerange/blob/master/DateTimeRange/DateTimeRange.cs
+// https://docs.microsoft.com/en-us/dotnet/api/system.datetime.compare?view=netcore-2.0#System_DateTime_Compare_System_DateTime_System_DateTime_
 
 using System;
 
@@ -18,23 +19,25 @@ namespace Itenso.TimePeriod {
     // ------------------------------------------------------------------------
     internal static class TimePeriodCalc {
 
-        // ----------------------------------------------------------------------
-       
-        public static bool HasInside (ITimePeriod period, DateTime? test) {
-            return period.Start <= test && period.End >= test;
+        // ----------------------------------------------------------------------      
+        public static bool HasInside (ITimePeriod period, DateTime test) {
+            var testStart = DateTime.Compare(test,period.Start);
+            var testEnd = DateTime.Compare(test,period.End);
+            if(testStart >= -1 && testEnd <= 1) return true;
+            return false;
         } // HasInside
 
         // ----------------------------------------------------------------------
         public static bool HasInside (ITimePeriod period, ITimePeriod test) {
-            return HasInside (period, test.Start) && HasInside (period, test.End);
+            var testStart = DateTime.Compare(test.Start,period.Start);
+            var testEnd = DateTime.Compare(test.End,period.End);
+            if(testStart >= -1 && testEnd <= 1) return true;
+            return false;            
         } // HasInside
 
         // ----------------------------------------------------------------------
         public static bool IntersectsWith (ITimePeriod period, ITimePeriod test) {
-            return
-            HasInside (period, test.Start) ||
-                HasInside (period, test.End) ||
-                (test.Start.Ticks < period.Start.Ticks && test.End.Ticks > period.End.Ticks);
+            return HasInside (period, test.Start) || HasInside (period, test.End);
         } // IntersectsWith
 
         // ----------------------------------------------------------------------
